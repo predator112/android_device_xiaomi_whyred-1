0bcbc477ec2acf2a3fa874e47aab13ae979dce86
dianlujitao <dianlujitao@lineageos.org>
2020-02-02 19:33:37 +0800

    whyred: Explictly build camera HIDL interfaces
     * The 64-bit variants are used by IMS blobs but no longer automatically
       built along with the 32-bit counterparts.
    
    Change-Id: I387e4345c3088bbaf55e24bcfe286ccd877507a6

--- device.mk
+++ device.mk
@@ -151,11 +151,13 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.device@3.4 \
     android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     libxml2 \
     Snap \
+    android.hardware.camera.provider@2.5 \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 # CNE
