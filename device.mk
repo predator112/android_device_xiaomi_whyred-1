245d42beb990bacdef0225386dd6b699f037f648
root2raj <ruturaj1999@gmail.com>
2020-05-08 10:48:53 +0530

    whyred: board: Nuke Snap

--- device.mk
+++ device.mk
@@ -160,7 +160,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     libxml2 \
-    Snap \
     android.hardware.camera.provider@2.5 \
     vendor.qti.hardware.camera.device@1.0.vendor
 
